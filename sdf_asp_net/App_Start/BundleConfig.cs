27cf811ac94b5304dbcccf431bb0e832e8a39a42
kevinw.develop <kevinw.develop@gmail.com>
2018-11-19 22:16:40 +0100

    implemented feature to add new projects and routed start up view to login

--- sdf_asp_net/App_Start/BundleConfig.cs
+++ sdf_asp_net/App_Start/BundleConfig.cs
@@ -23,7 +23,7 @@ namespace sdf_asp_net
                       "~/Scripts/bootstrap.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
+                      "~/Content/bootstrap-lumen.css",
                       "~/Content/site.css"));
         }
     }
---
+++ sdf_asp_net/ViewModels/ProjectsViewModel.cs
@@ -0,0 +1,13 @@
+﻿using sdf_asp_net.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace sdf_asp_net.ViewModels
+{
+    public class ProjectsViewModel
+    {
+        public List<Project> projects = new List<Project>();
+    }
+}
