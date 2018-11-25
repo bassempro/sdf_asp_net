ab3c764b13676181adeb41c5021fb798fa20c45f
Chamani Cheri <Chamani Cheri@CHAMANICHERI-PC>
2018-11-25 18:48:08 +0100

    CRUD for Projects and redesigned fontend

---
+++ sdf_asp_net/Models/ProjectModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel;
+
+namespace sdf_asp_net.Models
+{
+    public class ProjectModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        [DisplayName("Beschreibung")]
+        public string Description { get; set; }
+
+    }
+}
