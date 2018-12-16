73a99c2da202b146826fd3cbde431e21725284c1
Cihan <s0547588@hte-berlin.de>
2018-12-16 09:20:34 +0100

    ProjectChat!

--- sdf_asp_net/Hubs/ChatHub.cs
+++ sdf_asp_net/Hubs/ChatHub.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Web;   
 using Microsoft.AspNet.SignalR;
 using sdf_asp_net.Models;
@@ -14,18 +15,11 @@ namespace sdf_asp_net.Hubs
         {
             _context = new ApplicationDbContext();
         }
-        public override System.Threading.Tasks.Task OnConnected()
+        public void SendGroups(string name,string groupName, string message)
         {
-            Clients.All.user();
-            return base.OnConnected();
-        }
+            // Call the addNewMessageToPage method to update clients.
+            Clients.Group(groupName).addNewMessageToPage(name + ": " + message);
 
-        public void Send(string name, string message)
-        {
-            //wenn ich eine Message verschicke 
-            Clients.Caller.message("You: " + message);
-            //wenn andere eine Message verschickt
-            Clients.Others.message(message);
             ChatModel chatModel = new ChatModel()
             {
                 //init object
@@ -39,7 +33,34 @@ namespace sdf_asp_net.Hubs
             _context.SaveChanges();
 
 
+        }
+        public void JoinGroup(string name,string groupName)
+        {
+            Groups.Add(Context.ConnectionId, groupName);
+            Clients.Group(groupName).addNewMessageToPage($"{name} join [{groupName}]");
 
         }
+
+
+        public override Task OnConnected()
+        {
+            Clients.Caller.addNewMessageToPage("Connected");
+            return base.OnConnected();
+        }
+
+        public override Task OnDisconnected(bool stopCalled)
+        {
+            Clients.All.addNewMessageToPage("DisConnected");
+            return base.OnDisconnected(stopCalled);
+        }
+        public override Task OnReconnected()
+        {
+            Clients.All.addNewMessageToPage($"{Context.ConnectionId}: ReConnected");
+
+            return base.OnReconnected();
+        }
+
+
+       
     }
 }
