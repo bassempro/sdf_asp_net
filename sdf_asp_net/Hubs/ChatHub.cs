2d4ab214402e3007492c5a3c3a0c4489a79752ba
Cihan <s0547588@hte-berlin.de>
2019-01-20 05:19:06 +0100

    Chatfunktionen überarbeitet

--- sdf_asp_net/Hubs/ChatHub.cs
+++ sdf_asp_net/Hubs/ChatHub.cs
@@ -2,84 +2,103 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using System.Web;   
+using System.Web;
 using Microsoft.AspNet.SignalR;
-using sdf_asp_net.Models;
+using SignalRChat.Common;
 
-namespace sdf_asp_net.Hubs
+namespace SignalRChat
 {
     public class ChatHub : Hub
     {
-        ApplicationDbContext _context;
-        public ChatHub()
+        #region Data Members
+
+        static List<UserDetail> ConnectedUsers = new List<UserDetail>();
+        static List<MessageDetail> CurrentMessage = new List<MessageDetail>();
+
+        #endregion
+
+        #region Methods
+
+        public void Connect(string userName)
         {
-            _context = new ApplicationDbContext();
-        }
-
-        public void SendAll(string name, string message)
-        {
-            Clients.All.addNewMessageToAll(name + " : " + message);
-            Clients.All.isOnline(name + " : " + message);
-        }
-
-        public void SendPrivat(string name,string message,string connectionId)
-        {
-            Clients.Client(connectionId).privatChat(name + ": " + message);
-        }
-
-        public void SendGroups(string name,string groupName, string message)
-        {
-            
-            // Call the addNewMessageToPage method to update clients.
-            Clients.Group(groupName).addNewMessageToPage(name + ": " + message);
-            //Clients.Client(connectionId).privatChat(name + ": " + message);
+            var id = Context.ConnectionId;
 
 
-            ChatModel chatModel = new ChatModel()
+            if (ConnectedUsers.Count(x => x.ConnectionId == id) == 0)
             {
-                //init object
-                user = name,
-                message = message,
-                dateStamp = DateTime.Now
+                ConnectedUsers.Add(new UserDetail { ConnectionId = id, UserName = userName });
 
-            };
-            //add to DB and save changes
-            _context.ChatModels.Add(chatModel);
-            _context.SaveChanges();
+                // send to caller
+                Clients.Caller.onConnected(id, userName, ConnectedUsers, CurrentMessage);
 
+                // send to all except caller client
+                Clients.AllExcept(id).onNewUserConnected(id, userName);
 
-        }
-        public void JoinGroup(string name,string groupName)
-        {
-            Groups.Add(Context.ConnectionId, groupName);
-            Clients.Group(groupName).addNewMessageToPage($"{name} join [{groupName}]");
-            
+            }
 
         }
 
+        public void SendMessageToAll(string userName, string message)
+        {
+            // store last 100 messages in cache
+            AddMessageinCache(userName, message);
 
-        public override Task OnConnected()
+            // Broad cast message
+            Clients.All.messageReceived(userName, message);
+        }
+
+        public void SendPrivateMessage(string toUserId, string message)
         {
 
+            string fromUserId = Context.ConnectionId;
 
+            var toUser = ConnectedUsers.FirstOrDefault(x => x.ConnectionId == toUserId);
+            var fromUser = ConnectedUsers.FirstOrDefault(x => x.ConnectionId == fromUserId);
 
-            Clients.All.isOnline($"{Context.ConnectionId}");
-            return base.OnConnected();
+            if (toUser != null && fromUser != null)
+            {
+                // send to 
+                Clients.Client(toUserId).sendPrivateMessage(fromUserId, fromUser.UserName, message);
+
+                // send to caller user
+                Clients.Caller.sendPrivateMessage(toUserId, fromUser.UserName, message);
+            }
+
         }
 
         public override Task OnDisconnected(bool stopCalled)
         {
-            Clients.All.addNewMessageToPage("DisConnected");
+
+            var item = ConnectedUsers.FirstOrDefault(x => x.ConnectionId == Context.ConnectionId);
+            if (item != null)
+            {
+                ConnectedUsers.Remove(item);
+
+                var id = Context.ConnectionId;
+                Clients.All.onUserDisconnected(id, item.UserName);
+
+            }
+
             return base.OnDisconnected(stopCalled);
         }
-        public override Task OnReconnected()
-        {
-            Clients.All.addNewMessageToPage($"{Context.ConnectionId}: ReConnected");
 
-            return base.OnReconnected();
+
+        
+
+
+        #endregion
+
+        #region private Messages
+
+        private void AddMessageinCache(string userName, string message)
+        {
+            CurrentMessage.Add(new MessageDetail { UserName = userName, Message = message });
+
+            if (CurrentMessage.Count > 100)
+                CurrentMessage.RemoveAt(0);
         }
 
-
-       
+        #endregion
     }
+
 }
